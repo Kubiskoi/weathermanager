d707f458ec8efdb63309c33bbf9cc80163ea8842
Jakub <hoblikj@gmail.com>
2020-01-20 20:53:42 +0100

    basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TableComponent } from './table/table.component';
+import { ChartComponent } from './chart/chart.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/table' , pathMatch: 'full'},
+  { path: 'table', component: TableComponent },
+  { path: 'chart', component: ChartComponent },
+  { path: 'calculator', component: CalculatorComponent },
+  { path: '**', redirectTo: '/table' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
