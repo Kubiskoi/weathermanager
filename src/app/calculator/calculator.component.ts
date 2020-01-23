400936fe6e0aaa56b1ad7c467d25435a2a1db3f6
Jakub <hoblikj@gmail.com>
2020-01-23 07:49:24 +0100

    local storage logic in own service

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -4,6 +4,7 @@ import { MAT_RADIO_DEFAULT_OPTIONS } from '@angular/material/radio';
 import { CalcService } from '../services/calc.service';
 import { HeatIndex } from '../models/heatIndex.model';
 import { Subscription } from 'rxjs';
+import { LocalStorageService } from '../services/storage.service';
 
 @Component({
   selector: 'app-calculator',
@@ -18,9 +19,11 @@ export class CalculatorComponent implements OnInit, OnDestroy {
   heatIndex: HeatIndex;
   hasHeatIndexValue = false;
   heatIndexRecords: HeatIndex[] = [];
-  heatIndexRecordsSubscripion: Subscription;
+  // heatIndexRecordsSubscripion: Subscription;
 
-  constructor(private calcService: CalcService) { }
+  constructor(
+    private calcService: CalcService,
+    private localStorageService: LocalStorageService) { }
 
   ngOnInit() {
 
@@ -46,14 +49,16 @@ export class CalculatorComponent implements OnInit, OnDestroy {
           this.calcForm.controls['temp'].updateValueAndValidity();
         }
       }
-    )
-
-    this.heatIndexRecords = this.calcService.getHeatIndexRecords();
-    this.heatIndexRecordsSubscripion = this.calcService.recordsSubjectChange.subscribe(
-      (records: HeatIndex[]) => {
-        this.heatIndexRecords = records;
-      }
     );
+    
+    this.heatIndexRecords = this.localStorageService.getHeatIndexes();
+
+    // this.heatIndexRecords = this.calcService.getHeatIndexRecords();
+    // this.heatIndexRecordsSubscripion = this.calcService.recordsSubjectChange.subscribe(
+    //   (records: HeatIndex[]) => {
+    //     this.heatIndexRecords = records;
+    //   }
+    // );
   }
 
   onSubmit() {
@@ -66,6 +71,9 @@ export class CalculatorComponent implements OnInit, OnDestroy {
     );
 
     this.hasHeatIndexValue = true;
+
+    this.localStorageService.store(this.heatIndex);
+    this.heatIndexRecords = this.localStorageService.getHeatIndexes();
   }
 
   unitMin(control: FormControl): {[s: string]: boolean} {    
@@ -81,10 +89,8 @@ export class CalculatorComponent implements OnInit, OnDestroy {
   }
 
   onClearHIrecords() {
-    this.calcService.clearStorage();
+    this.localStorageService.clearStorage();
+    this.heatIndexRecords = [];
   }
 
-  ngOnDestroy() {
-    this.heatIndexRecordsSubscripion.unsubscribe();
-  }
 }
--- src/app/services/calc.service.ts
+++ src/app/services/calc.service.ts
@@ -1,22 +1,10 @@
-import { Injectable, Inject, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HeatIndex } from '../models/heatIndex.model';
-import { LOCAL_STORAGE, StorageService } from 'angular-webstorage-service';
-import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CalcService {
-  private heatIndexRecords: HeatIndex[] = [];
-  private STORAGE_KEY = 'local_hi';
-  public recordsSubjectChange = new Subject<HeatIndex[]>(); 
-
-  constructor(@Inject(LOCAL_STORAGE) private storage: StorageService) { }
-
-  getHeatIndexRecords() {
-    this.heatIndexRecords = this.storage.get(this.STORAGE_KEY) || [];
-    return this.heatIndexRecords.slice();
-  }
 
   calcHeatIndex(temp:number, unit: string, rh: number): HeatIndex {
     let resp: HeatIndex = {inC:0, inF: 0, status: ''};
@@ -32,28 +20,11 @@ export class CalcService {
     resp.inC = this.FtoC(resp.inF);
     resp.status = this.assignStatus(resp.inF);
     
-    this.store(resp);
-
     return resp;
   }
 
   doCalc(T:number, rh: number) {
     return (-42.379) + (2.04901523 * T) + (10.14333127 * rh) - (0.22475541 * T * rh) - (6.83783e-3 * T**2) - (5.481717e-2 * rh**2) + (1.22874e-3 * T**2 * rh) + (8.5282e-4 * T * rh**2) - (1.99e-6 * T**2 * rh**2);
-  }
-
-  store(hi: HeatIndex) {
-    this.heatIndexRecords = this.storage.get(this.STORAGE_KEY) || [];
-    
-    if (this.heatIndexRecords.length == 5) {
-      this.heatIndexRecords.pop();
-    }
-    
-    this.heatIndexRecords.unshift(hi);
-    this.storage.set(this.STORAGE_KEY, this.heatIndexRecords);
-    
-    this.recordsSubjectChange.next(
-      this.storage.get(this.STORAGE_KEY) || []
-    );
   }
 
   assignStatus(heatIndex: number): string {
@@ -70,12 +41,5 @@ export class CalcService {
 
   FtoC(tempF: number): number {
     return +(((tempF - 32) / 1.8).toFixed(2));
-  }
-
-  clearStorage() {
-    this.storage.remove(this.STORAGE_KEY);
-    this.recordsSubjectChange.next(
-      this.storage.get(this.STORAGE_KEY) || []
-    );
   }
 }
